4515e307a00845811aee76a3e2134312b4e1d684
Riva Adara Ridley <rivaridley1@Riva-Ridleys-MacBook-Pro.local>
2016-02-18 13:28:33 -0800

    Timer CSS.

--- js/main.jsx
+++ js/main.jsx
@@ -15,14 +15,62 @@ var Quiz = React.createClass({
     </header>
 
     <div className='main'>
+      <div className='timercontainer'><Timer /></div>
       <button className='testbutton'><span>TAKE TEST</span></button>
       <button className='testbutton'><span>BEGIN EVALUATION</span></button>
     </div>
+
+
 
   </div>
 
     )
   }
+});
+
+var Timer = React.createClass({
+
+  getInitialState: function(){
+    return {
+      secondsToElapse:  60
+    }
+  },
+
+  //return minutes and seconds in seperate functions
+  renderMinutes: function(){
+
+  },
+
+  renderSeconds: function(){
+
+  },
+
+  resetTimer: function(){
+   clearInterval(this.interval);
+   this.setState({ secondsToElapse: 0 });
+   this.start();
+ },
+
+  tick: function(){
+  this.setState({secondsToElapse: this.state.secondsElapsed - 1 });
+  },
+
+  start: function(){
+      this.interval = setInterval(this.tick, 1000);
+  },
+
+  componentDidMount: function(){
+   setTimeout(this.start, this.props.timeout);
+  },
+
+  render: function(){
+    return (
+      <div className='timer'>
+        00:{this.state.secondsElapsed}
+      </div>
+    )
+ },
+
 });
 
 ReactDOM.render(<Quiz />, document.querySelector('#quiz-app'));
